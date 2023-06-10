45980e230a9c8a95f647e364bfb1c5ef6774822a
vishwa <viswasakthi2@gmail.com>
2023-06-10 13:09:39 +0530

    1

--- background.js
+++ background.js
@@ -1,53 +1,25 @@
-// This is a simplified version of the background.js file from the Simkl repository.
-// It only includes the code necessary to fetch the watch history from Netflix and send it to our server.
-
-var timer = null,
-    timerStream = 30 * 60 * 1000, // 30 mins
-    lastChecked = moment();
-
-var debug  = false, statusCallback = null,
-    storage = storageClass(),
-    netflix = netflixApi(statusUpdate, sendMessage);
-
-(function onStart() {
-    if (!storage.isInitiated()) { setTimeout(onStart,500); return;}
-
-    timer = setInterval(timerUpdate, timerStream); // 30 mins
-})();
-
-function statusUpdate(provider, active) {
-    if (netflix === undefined) return;
-
-    let status = 'waiting';
-
-    if (netflix.checking()) status = 'updating';
-
-    if (statusCallback) statusCallback(status,provider);
-
-    if (provider === 'oauth' && active === 'logged') getWatchHistory();
-    setIcon();
-}
-
-function sendMessage(tabId,message,callback){chrome.tabs.sendMessage(tabId, message, callback);}
-
-function timerUpdate() {
-    lastChecked = new Date();
-    if (storage.authorized){
-        log('do Checking');
-        getWatchHistory();
-    }
-}
-
-function getWatchHistory() {
-    if (netflix.enabledExport()) netflix.checkUpdates();
-}
-
-chrome.runtime.onMessage.addListener(function (request, sender, response) {
-    if (request.type === 'netflix'){
-        if (request.action === 'load') {netflix.updateCache();
-        response && response({grey: netflix.enabledGrey(), secret: netflix.enabledSecret()});}
-        if (request.action === 'recvItems' && storage.authorized && (netflix.enabledRatings() || netflix.enabledGrey())) netflix.fetchItems(request.items,sender.tab);
-        if (request.action === 'getRatings' && netflix.enabledRatings()) netflix.getRatings({id: request.items,tab: sender.tab.id});
-        if (request.action === 'getRandom')   netflix.getRandom({url: request.items,tab: sender.tab.id});
-    }
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostEquals: 'www.netflix.com'},
+                })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
 });
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: fetchNetflixHistory,
+    });
+});
+
+function fetchNetflixHistory() {
+    chrome.tabs.executeScript({
+        code: 'fetchNetflixData();'
+    });
+}
--- content.js
+++ content.js
@@ -1,38 +1,26 @@
-// This script will be injected into the Netflix viewing activity page.
-// It can read the DOM of the page to fetch the watch history.
+function fetchNetflixData() {
+    var watched = Array.from(document.querySelectorAll(".retableRow")).map(function(row) {
+        var date = row.querySelector(".date").textContent;
+        var text = row.querySelector(".title").textContent;
+        text = text.replace(": Part ", " - Part ");
 
-function decode_date(date) {
-    // Function to convert Netflix date format to a JavaScript Date object.
-    // Netflix dates are in the format 'Month DD, YYYY'
-    const [month, day, year] = date.split(' ');
-    return new Date(`${month} ${day.replace(',', '')}, ${year}`);
-  }
-  
-  // Load the list of watched shows from Netflix.
-  var watched = Array.from(document.querySelectorAll(".retableRow")).map(function(row) {
-    var date = row.querySelector(".date").textContent;
-    var text = row.querySelector(".title").textContent;
-    text = text.replace(": Part ", " - Part ");
-    if(text.indexOf("Mystery Science Theater 3000: The Return: Season ") !== -1)
-      text = text.replace(": The Return: Season ", ": Season 1");
-  
-    var [show, season, title] = text.split(/: Season |: Series |: Collection |: "/);
-    var isShow = !title ? false : true;
-    title = isShow ? title.replace(/^(.*)\"$/, "$1") : row.querySelector(".title").textContent;
-    season = parseInt(season);
-  
-    show = show.replace(" (U.S.)", "");
-    date = decode_date(date);
-  
-    return {
-      date: date,
-      isShow: isShow,
-      show: show,
-      season: season,
-      title: title
-    };
-  });
-  
-  // Send the watched data to the background script
-  chrome.runtime.sendMessage({watched: watched});
-  
+        var [show, season, title] = text.split(/: Season |: Series |: Collection |: "/);
+        var isShow = !title ? false : true;
+        title = isShow ? title.replace(/^(.*)\"$/, "$1") : row.querySelector(".title").textContent;
+        season = parseInt(season);
+
+        show = show.replace(" (U.S.)", "");
+        date = new Date(date);
+
+        return {
+            date: date,
+            isShow: isShow,
+            show: show,
+            season: season,
+            title: title
+        };
+    });
+
+    // Send the data to the background script
+    chrome.runtime.sendMessage({action: "fetchNetflixData", data: watched});
+}
